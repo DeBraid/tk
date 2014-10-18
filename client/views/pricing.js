1a4fe312f8246f1aa953034df4b48d8977575b55
DeBraid <de.braid@gmail.com>
2014-10-18 11:02:16 -0400

    styled pricing page

--- client/views/pricing.js
+++ client/views/pricing.js
@@ -1,24 +1,24 @@
 Template.pricing.helpers({
   services: function () {
     return [
-      {service: "Make up tutorial", price: 40},
-      {service: "Facial treatment", price: 75},
-      {service: "Manicure", price: 30},
-      {service: "Pedicure", price: 50},
-      {service: "Shellac Manicure", price: 40},
-      {service: "Shellac Pedicure", price: 60},
-      {service: "eyebrows", price: 15},
-      {service: "upper lip", price: 10},
-      {service: "other facial waxing", price: 10},
-      {service: "underarms", price: 15},
-      {service: "half arm", price: 20},
-      {service: "hands", price: 8},
-      {service: "bikini", price: 25},
-      {service: "brazillian", price: 50},
-      {service: "back", price: 50},
-      {service: "half leg", price: 25},
-      {service: "full leg", price: 40},
-      {service: "feet", price: 8}
+      {service: "Make Up Tutorial", price: 40, type: "general", klass: "success"},
+      {service: "Facial Treatment", price: 75, type: "general", klass: "success"},
+      {service: "Manicure", price: 30, type: "nails", klass: "info"},
+      {service: "Pedicure", price: 50, type: "nails", klass: "info"},
+      {service: "Shellac Manicure", price: 40, type: "nails", klass: "info"},
+      {service: "Shellac Pedicure", price: 60, type: "nails", klass: "info"},
+      {service: "Eyebrows", price: 15, type: "waxing", klass: "warning"},
+      {service: "Upper Lip", price: 10, type: "waxing", klass: "warning"},
+      {service: "Other Facial Waxing", price: 10, type: "waxing", klass: "warning"},
+      {service: "Underarms", price: 15, type: "waxing", klass: "warning"},
+      {service: "Half Arm", price: 20, type: "waxing", klass: "warning"},
+      {service: "Hands", price: 8, type: "waxing", klass: "warning"},
+      {service: "Bikini", price: 25, type: "waxing", klass: "warning"},
+      {service: "Brazillian", price: 50, type: "waxing", klass: "warning"},
+      {service: "Back", price: 50, type: "waxing", klass: "warning"},
+      {service: "Half Leg", price: 25, type: "waxing", klass: "warning"},
+      {service: "Full Leg", price: 40, type: "waxing", klass: "warning"},
+      {service: "Feet", price: 8, type: "waxing", klass: "warning"}
     ]
   }
 });
